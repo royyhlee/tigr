b07c54f1b545cd3708a7b84746ccec91aab83ec6
Roy Lee <royyhlee@gmail.com>
2025-05-18 22:15:55 -0700

    wip

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,25 +1,35 @@
 <script lang="ts">
     import { invoke } from "@tauri-apps/api/core";
+    import { watch } from "@tauri-apps/plugin-fs";
+
+    const repoPath = "/Users/royyhlee/Projects/yumz";
 
     let status = $state("");
-    async function gitStatus(event: Event) {
-        status = await invoke("git_status", {
-            repoPath: "/Users/royyhlee/Projects/yumz",
-        });
+    async function gitStatus() {
+        status = await invoke("git_status", { repoPath });
 
         console.log({ status });
     }
 
     let branches = $state<string[]>([]);
     async function gitBranches() {
-        const result: string = await invoke("git_branches", {
-            repoPath: "/Users/royyhlee/Projects/yumz",
-        });
+        const result: string = await invoke("git_branches", { repoPath });
 
         branches = result.split("\n").filter((branch) => branch.length > 0);
 
         console.log({ branches });
     }
+
+    watch(
+        repoPath,
+        (event) => {
+            console.log("File changed:", event);
+        },
+        {
+            delayMs: 1000,
+            recursive: true,
+        },
+    );
 </script>
 
 <main class="container">
