5c4559f7ccb14e1107f62ab9816ca9eb533e5b5a
Roy Lee <royyhlee@gmail.com>
2025-05-18 18:18:33 -0700

    Added fs plugin

--- src-tauri/src/lib.rs
+++ src-tauri/src/lib.rs
@@ -14,6 +14,7 @@ fn git_branches(repo_path: String) -> Result<String, String> {
 #[cfg_attr(mobile, tauri::mobile_entry_point)]
 pub fn run() {
     tauri::Builder::default()
+        .plugin(tauri_plugin_fs::init())
         .plugin(tauri_plugin_opener::init())
         .invoke_handler(tauri::generate_handler![git_status, git_branches])
         .run(tauri::generate_context!())
