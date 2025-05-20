559083b342feeeb6d01553a965f877b847b3818e
Roy Lee <ylee@costar.com>
2025-05-20 15:54:55 -0700

    Git branch

--- src/lib/git.ts
+++ src/lib/git.ts
@@ -4,7 +4,7 @@ import { type GitCommand } from "./git-commands";
 export default function git(params: {repoPath: string, cmd: GitCommand, args?: string[]}) {
   const { repoPath, cmd,args } = params;
 
-  return invoke("git", {
+  return invoke<string>("git", {
     repoPath,
     args: [cmd, ...(args ?? [])],
   });
--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,20 +1,26 @@
 <script lang="ts">
     import git from "../lib/git";
 
-    const repoPath = 'C:/Users/ylee/Projects/sandbox/apt-git-tool';
+    let repoPath = $state('C:/Users/ylee/Projects/sandbox/apt-git-tool');
+    let branches = $state<string[]>([]);
 
     async function getBranches() {
         try {
-            const branches = await git({ repoPath, cmd: 'branch', args: ['--all'] });
-            console.log(branches);
+            const result = await git({ repoPath, cmd: 'branch', args: ['--all'] });
+            branches = result.split('\n').map(branch => branch.trim()).filter(branch => branch);
         } catch (error) {
             console.error('Error fetching branches:', error);
         }
     }
+
 </script>
 
 <main class="container">
+    <p>{repoPath}</p>
+    {#each branches as branch}
+        <p>{branch}</p>
+    {/each}
     <button type="button" onclick={getBranches}>Branches</button>
 </main>
 
-<style></style>
+<style></style> 
