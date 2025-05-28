52c65410f671fdc72abb074206476706a7b07e80
Roy Lee <ylee@costar.com>
2025-05-28 16:51:20 -0700

    Formatting

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -5,9 +5,9 @@
     import { load, Store } from "@tauri-apps/plugin-store";
     import { onMount } from "svelte";
 
-    let repoPath = $state('');
+    let repoPath = $state("");
     let branches = $state<string[]>([]);
-    let message = $state('');
+    let message = $state("");
     let git: Git | null = null;
     let selectedRepo: string;
     let repositories: string[] = [];
@@ -15,27 +15,27 @@
     let config: GitToolConfig | undefined = undefined;
 
     onMount(async () => {
-        store = await load('store.json', { autoSave: true });
+        store = await load("store.json", { autoSave: true });
         console.log(store);
-        config = await store?.get<GitToolConfig>('git-tool-store');
+        config = await store?.get<GitToolConfig>("git-tool-store");
         console.log(config);
-        repoPath = config?.selected || '';
-        git = await Git.create(config?.selected || '');
+        repoPath = config?.selected || "";
+        git = await Git.create(config?.selected || "");
         await getBranches();
     });
 
     async function getBranches() {
         if (!git) {
-            message = 'No repository selected';
+            message = "No repository selected";
             return;
         }
 
-        let { result, error } = await git.getBranches(['--all']);
+        let { result, error } = await git.getBranches(["--all"]);
         if (error) {
             message = `Error: ${error}`;
         } else {
             branches = result;
-            message = `Branches: ${result.join(', ')}`;
+            message = `Retrieved branches`;
         }
     }
 
@@ -43,35 +43,45 @@
         const file = await open({
             multiple: false,
             directory: true,
-            title: 'Add Repository',
+            title: "Add Repository",
         });
 
-        if (!file || typeof file !== 'string') return;
+        if (!file || typeof file !== "string") return;
 
-        const path = (file as string).replaceAll('\\', '\/');
+        const path = (file as string).replaceAll("\\", "/");
         const isGitRepo = await Git.isGitRepository(path);
 
         if (isGitRepo) {
             repoPath = path;
-            await store?.set('git-tool-store', { selected: path });
+            await store?.set("git-tool-store", { selected: path });
             message = `Added repository: ${path}`;
+
+            await getBranches();
         } else {
             message = `Not a valid git repository: ${path}`;
         }
     }
-
 </script>
 
 <main class="container">
-    <p>{ message }</p>
+    <p>{message}</p>
     <p>{repoPath}</p>
 
-    {#each branches as branch}
-        <p>{branch}</p>
-    {/each}
+    <div class="branches">
+        {#each branches as branch}
+            <p>{branch}</p>
+        {/each}
+    </div>
 
     <button type="button" onclick={getBranches}>Branches</button>
     <button type="button" onclick={addRepository}>Add Repository</button>
 </main>
 
-<style></style> 
+<style>
+    .branches {
+        overflow-y: auto;
+        height: 200px;
+        border: 1px solid #ccc;
+        padding: 10px;
+    }
+</style>
